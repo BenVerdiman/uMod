e2edddd3066ae48c1954efdbc5d70b433b62de8d
BenVerdiman <49784615+BenVerdiman@users.noreply.github.com>
2021-12-11 13:59:42 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const ifmsg = "No";
+const msg = "No";
+const { token } = require('./config.json');
+
+
+
+client.once("ready", () => {
+	console.log('Hello');
+});
+
+client.on("message", message => {
+  function sendif(ifmsg, msg){
+  if(message.content == ifmsg){
+    message.channel.send(msg);
+  }
+};
+  console.log(message.content);
+  if(message.content == "uban"){
+    message.channel.send("You sus!");
+  }
+  if(message.content == "ukick"){
+    message.channel.send("Kick ya!");
+  }
+  sendif("umod", "U the mod!");
+  
+  if(message.author.tag == 'LencherOfficial#0001'){
+    sendif("uhecker", "no u");
+  }
+});
+client.login(token);
